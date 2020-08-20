27640406324fb7c044387b2e8d1f44a6be390e57
Jayanta Biswas <bjayanta.me@gmail.com>
2020-08-20 17:28:44 +0600

    Connect to Mongo DB.

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config().parsed
+}
+
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const indexRouter = require('./routes/index')
+const mongoose = require('mongoose')
 
 // configure 
 app.set('view engine', 'ejs')
@@ -13,5 +18,16 @@ app.use(express.static('public'))
 
 app.use('/', indexRouter)
 
+// mongo db connection
+mongoose.connect(process.env.DATABASE_URI, { 
+    useUnifiedTopology: true, 
+    useNewUrlParser: true 
+})
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose.'))
+
 // listener
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server started on port 3000.')
+})
